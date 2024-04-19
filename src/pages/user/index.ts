91a9fa84bc8f3e0ede305515f4ae1c39812b805a
PavelZabolotnikov <zabolotnikovpg2@mail.ru>
2024-04-19 13:21:25 +0300

    fix: устранены замечания

--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -5,11 +5,11 @@ export { UserPasswordChangePage } from './user-password-change';
 import myPhoto from '../../assets/photo/Bortnikov.png';
 import Handlebars from 'handlebars';
 Handlebars.registerHelper('userInfo', () => ({
-  photo: myPhoto,
-  email: 'yandex@yandex.ru',
-  login: 'FSBman',
-  second_name: 'Бортников',
   first_name: 'Александр',
-  chat_name: 'BigMan',
-  phone_number: '8-495-224-22-22',
+  second_name: 'Бортников',
+  display_name: 'BigMan',
+  login: 'FSBman',
+  email: 'yandex@yandex.ru',
+  phone: '8-495-224-22-22',
+  photo: myPhoto,
 }));
