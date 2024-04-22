12b65735412479008fed9a07d8c44bb493e0a66a
Pavel <zabolotnikovpg2@mail.ru>
2024-04-22 22:40:19 +0300

    feat: добавлена валидация

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,5 +1,9 @@
 const login = /^[a-zA-Z_-]{3,20}\D$/;
 const password = /^(?=.*[A-Z])(?=.*\d).{8,40}$/;
+const email = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+const name = /^([А-ЯA-Z][а-яa-z-]*)$/;
+const phone = /^\+?\d{10,15}$/;
+const message = /^.*$/;
 
 export const validation = (type: string, value: string, ): boolean => {
   switch (type) {
@@ -7,6 +11,14 @@ export const validation = (type: string, value: string, ): boolean => {
       return login.test(value);
     case 'password':
       return password.test(value);
+      case 'email':
+        return email.test(value);
+      case 'name':
+        return name.test(value);
+      case 'phone':
+        return phone.test(value);
+        case 'message':
+          return message.test(value);
     default:
       return false;
   }
