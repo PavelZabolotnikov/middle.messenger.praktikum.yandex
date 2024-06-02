290842fbee396c7ec7f326423f181c4193f58cef
Pavel <zabolotnikovpg2@mail.ru>
2024-06-02 20:51:56 +0300

    fix: поправлена ошибка в файле profile.ts

--- src/utils/types/auth.ts
+++ src/utils/types/auth.ts
@@ -22,4 +22,3 @@ export interface SignInData {
     login: string;
     password: string;
   }
-  
--- src/utils/types/profile.ts
+++ src/utils/types/profile.ts
@@ -12,4 +12,3 @@ export interface Password {
     oldPassword: string;
     newPassword: string;
   }
- 
